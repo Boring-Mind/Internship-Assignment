6b19cf348e2ec5b9f22b1dff383a175eab5f9a51
Boring-Mind <super.d3280@ya.ru>
2020-08-26 09:54:42 -0700

    feet: added console email backend

--- emails/forms.py
+++ emails/forms.py
@@ -5,23 +5,6 @@ from django.urls import reverse
 
 
 class EmailForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id = 'id-EmailForm'
-        self.helper.form_class = 'form-group'
-        self.helper.form_method = 'post'
-        self.helper.form_action = reverse('index')
-
-        self.helper.layout = layout.Layout(
-            layout.ButtonHolder(
-                layout.Submit(
-                    'submit',
-                    'Submit',
-                ),
-            )
-        )
-        
     subject = forms.CharField(
         max_length=120,
         required=False,
@@ -31,6 +14,11 @@ class EmailForm(forms.Form):
         max_length=1000,
         widget=forms.widgets.Textarea
     )
-    sender = forms.EmailField(
-        required=False, widget=forms.HiddenInput
-    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-EmailForm'
+        self.helper.form_class = 'form-group'
+        self.helper.form_method = 'post'
+        self.helper.form_action = reverse('index')
--- emails/views.py
+++ emails/views.py
@@ -1,5 +1,7 @@
+from django.core import mail
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
+from users.models import User
 
 from .forms import EmailForm
 
@@ -12,3 +14,21 @@ class EmailFormView(FormView):
     template_name = 'index.html'
     form_class = EmailForm
     success_url = reverse_lazy('success')
+
+    def send_email(self, form):
+        subject = form.cleaned_data['subject']
+        sender = self.request.user.email
+        message = form.cleaned_data['message']
+        receivers = User.objects.only("email").values()
+        receivers = [r['email'] for r in receivers if bool(r['email'])]
+
+        if not bool(receivers):
+            # If receivers list is empty
+            return
+        
+        email = mail.EmailMessage(subject, message, sender, receivers)
+        email.send()
+
+    def form_valid(self, form):
+        self.send_email(form)
+        return super().form_valid(form)
